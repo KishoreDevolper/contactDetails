8769b0168711a169d39ab30afd47ebc95f78ce39
kishoreDevolper <kishore.t@doodleblue.in>
2023-07-11 18:09:21 +0530

    code update

--- src/Components/Sidenav.js
+++ src/Components/Sidenav.js
@@ -1,11 +1,11 @@
-import React,{Component}from "react";
+import React, { Component } from "react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import "./navbar.css";
 
-import {FaHouseUser,FaCopy,FaIdBadge,FaPhone,FaCalendarDay,FaClock,FaUserAlt} from "react-icons/fa"
+import { FaHouseUser, FaCopy, FaIdBadge, FaPhone, FaCalendarDay, FaClock, FaUserAlt } from "react-icons/fa"
 
 
 
@@ -13,33 +13,33 @@ export class Sidenav extends Component {
   render() {
     return (
       <div>
-         <div className="sideNav pt-2  bg-danger bg-gradient text-white">
-                        <div className="navIcon">
-                            <FaHouseUser />
-                        </div>
-                        <div className="navIcon">
-                            <FaIdBadge />
-                        </div>
-                        <div className="navIcon">
-                            <FaCopy />
-                        </div>
-                        <div className="navIcon">
-                            <FaPhone />
-                        </div>
-                        <div className="navIcon">
-                            <FaCalendarDay />
-                        </div>
-                        <div className="navIcon">
-                            <FaClock />
-                        </div>
-                        <div className="navIcon">
-                            <FaUserAlt />
-                        </div>
-                    </div>
+        <div className="sideNav pt-2  bg-danger bg-gradient text-white">
+          <div className="navIcon">
+            <FaHouseUser />
+          </div>
+          <div className="navIcon">
+            <FaIdBadge />
+          </div>
+          <div className="navIcon">
+            <FaCopy />
+          </div>
+          <div className="navIcon">
+            <FaPhone />
+          </div>
+          <div className="navIcon">
+            <FaCalendarDay />
+          </div>
+          <div className="navIcon">
+            <FaClock />
+          </div>
+          <div className="navIcon">
+            <FaUserAlt />
+          </div>
+        </div>
       </div>
-    
-  );
-}
+
+    );
+  }
 }
 
 export default Sidenav;
